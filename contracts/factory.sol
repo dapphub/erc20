b5952df663ee909abf39a5ac012899ed4857765f
Nikolai Mushegian <nikolai@nexusdev.us>
2016-10-19 17:16:59 +0000

    dapple/solc upgrade

--- contracts/factory.sol
+++
@@ -1,9 +0,0 @@
-import 'base.sol';
-
-contract ERC20BaseFactory {
-    function createERC20Base(uint initial_balance) returns (ERC20Base b) {
-        b = new ERC20Base(initial_balance);
-        b.transfer(msg.sender, initial_balance);
-        return b;
-    }
-}
