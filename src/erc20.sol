fc53f48470165e34b5d04623265f57671de57d18
Nikolai Mushegian <nikolai@nexusdev.us>
2017-02-02 14:47:57 -0500

    reduce repo to interface - base implementations moved to ds-token

--- src/erc20.sol
+++ src/erc20.sol
@@ -14,23 +14,20 @@
    limitations under the License.
 */
 
-pragma solidity ^0.4.2;
+pragma solidity ^0.4.9;
 
 // Token standard API
 // https://github.com/ethereum/EIPs/issues/20
 
-contract ERC20Constant {
+contract ERC20 {
     function totalSupply() constant returns (uint supply);
     function balanceOf( address who ) constant returns (uint value);
-    function allowance(address owner, address spender) constant returns (uint _allowance);
-}
-contract ERC20Stateful {
+    function allowance( address owner, address spender ) constant returns (uint _allowance);
+
     function transfer( address to, uint value) returns (bool ok);
     function transferFrom( address from, address to, uint value) returns (bool ok);
-    function approve(address spender, uint value) returns (bool ok);
-}
-contract ERC20Events {
-    event Transfer(address indexed from, address indexed to, uint value);
+    function approve( address spender, uint value ) returns (bool ok);
+
+    event Transfer( address indexed from, address indexed to, uint value);
     event Approval( address indexed owner, address indexed spender, uint value);
 }
-contract ERC20 is ERC20Constant, ERC20Stateful, ERC20Events {}
