1050ba1bff839b8823730c1dd55ba44f0beae987
Andy Milenius <andrew.milenius@gmail.com>
2017-05-10 16:11:17 -0400

    update license

--- src/erc20.sol
+++ src/erc20.sol
@@ -1,5 +1,5 @@
 /*
-   Copyright 2016 Nexus Development, LLC
+   Copyright 2017 DappHub, LLC
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
