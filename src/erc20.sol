f322aaca414db343337814097d2af43214bee96c
rain <rainbreak@riseup.net>
2018-11-28 13:02:47 +0000

    solc v0.5.0

--- src/erc20.sol
+++ src/erc20.sol
@@ -6,7 +6,7 @@
 // required for copyright to apply.  As a result, this is free and
 // unencumbered software belonging to the public domain.
 
-pragma solidity ^0.4.8;
+pragma solidity >0.4.20;
 
 contract ERC20Events {
     event Approval(address indexed src, address indexed guy, uint wad);
