3994e351b28da57b3b396148648efb88aaaa26ab
James Foley <james.foley@protonmail.com>
2020-04-24 14:42:04 -0700

    modified to work with solc 0.6.6

--- src/erc20.sol
+++ src/erc20.sol
@@ -13,14 +13,12 @@ contract ERC20Events {
     event Transfer(address indexed src, address indexed dst, uint wad);
 }
 
-contract ERC20 is ERC20Events {
-    function totalSupply() public view returns (uint);
-    function balanceOf(address guy) public view returns (uint);
-    function allowance(address src, address guy) public view returns (uint);
+abstract contract ERC20 is ERC20Events {
+    function totalSupply() public virtual view returns (uint);
+    function balanceOf(address guy) public virtual view returns (uint);
+    function allowance(address src, address guy) public virtual view returns (uint);
 
-    function approve(address guy, uint wad) public returns (bool);
-    function transfer(address dst, uint wad) public returns (bool);
-    function transferFrom(
-        address src, address dst, uint wad
-    ) public returns (bool);
+    function approve(address guy, uint wad) public virtual returns (bool);
+    function transfer(address dst, uint wad) public virtual returns (bool);
+    function transferFrom( address src, address dst, uint wad) public virtual returns (bool);
 }
